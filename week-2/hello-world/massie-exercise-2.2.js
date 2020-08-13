cea37ca73cd325d7b52fb3f164c6af331b59cd18
Sarah Massie <pxchinko@gmail.com>
2020-08-12 20:40:26 -0700

    master

---
+++ week-2/hello-world/massie-exercise-2.2.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var http = require("http");
+var app = express();
+
+app.use(function(request, response){
+    console.log("In comes a request to: " + request.url);
+    response.end("Hello World\n");
+}) 
+
+http.createServer(app).listen(8080, function() {
+    console.log('Application has started on port %s', 8080)
+});
