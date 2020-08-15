1aa3c66d4422c62d8c94fe957301b2ab9db1fad1
Sarah Massie <pxchinko@gmail.com>
2020-08-14 21:31:50 -0700

    master

--- week-2/routes/massie-assignment-2.3.js
+++ week-2/routes/massie-assignment-2.3.js
@@ -6,20 +6,20 @@ var http = require("http");
 var app = express();
 
 app.get("/", function(request, response) {
-    response.end("Welcome to the homepage!");
+    response.end("Welcome to the homepage!"); // homepage response
 });
 
 app.get("/about", function(request, response) {
-    response.end("Welcome to the about page!");
+    response.end("Welcome to the about page!"); // about page response
 });
 
 app.get("/contact", function(request, response) {
-    response.end("Welcome to the contact page!");
+    response.end("Welcome to the contact page!"); // contact page response
 });
 
 app.use( function(request, response) {
-    response.statusCode = 4-4;
-    response.end("404!");
+    response.statusCode = 404;
+    response.end("404!"); // 404 page response
 });
 
 http.createServer(app).listen(8080);
