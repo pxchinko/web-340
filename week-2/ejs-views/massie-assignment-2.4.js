f4f07c9a5c53bf7e7e2cfee53bbe2798620f72e7
Sarah Massie <pxchinko@gmail.com>
2020-08-16 23:04:33 -0700

    master

--- week-2/ejs-views/massie-assignment-2.4.js
+++ week-2/ejs-views/massie-assignment-2.4.js
@@ -3,20 +3,20 @@ var express = require("express");
 var path = require("path");
 var app = express();
 
-app.set("views", path.resolve(_dirname,"views"));
-// tell express the views are in the 'views' directory
+app.set("views", path.resolve(__dirname, "views"))
+// tells express the views are in the "views" directory
 
 app.set("view engine", "ejs");
 // tell express to use the ejs view engine
 
-app.get("/", function(request, response) {
-    response.render("index.ejs", {
+app.get("/", function(request, person) {
+    person.render("index", {
         firstName: "Sarah",
         lastName: "Massie",
         address: "Orange County"
     });
 });
 
-http.createServer(app).listen(8080, function() {
-    console.log("EJS-Views app started on port 8080.");
+http.createServer(app).listen(3000, function() {
+    console.log("EJS-Views app started on port 3000.")
 });
