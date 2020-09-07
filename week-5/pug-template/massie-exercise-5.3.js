bc33c86ada38e9d559abf326c5a55e85c2b25e74
Sarah Massie <pxchinko@gmail.com>
2020-09-06 22:59:11 -0700

    master

--- week-5/pug-template/massie-exercise-5.3.js
+++ week-5/pug-template/massie-exercise-5.3.js
@@ -1,3 +1,12 @@
+/*
+============================================
+; Title: massie-exercise-5.3.js
+; Author: Professor Krasso
+; Date: September 6 2020
+; Modified By: Sarah Massie
+; Description: Making a Pug template
+;===========================================
+*/
 var express = require("express");
 var path = require("path");
 var http = require("http");
@@ -10,7 +19,7 @@ app.set("view engine", "pug");
 
 app.get("/", function(request, response) {
     response.render("index", {
-        message: "Welcome to my Pug based homepage!"
+        message: "This is a Pug template page"
     });
 });
 
