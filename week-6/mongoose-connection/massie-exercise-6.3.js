366681537a8c7408e7d7e11a77ef4d629fea38ce
Sarah Massie <pxchinko@gmail.com>
2020-09-11 13:20:59 -0700

    master

---
+++ week-6/mongoose-connection/massie-exercise-6.3.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var mongoose = require("mongoose");
+var http = require("http");
+var logger = require("morgan");
+
+var mongoDB = "mongodb+srv://admin:MonkeyPassword@buwebdev-cluster-1.eate3.mongodb.net/test";
+mongoose.connect(mongoDB, {
+    useMongoClient: true
+});
+
+mongoose.Promise = global.Promise;
+
+var db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", function() {
+    console.log("Application connected to mLab MongoDB instance")
+})
+
+var app = express();
+app.use(logger('dev'));
+
+http.createServer(app).listen(5000, function() {
+    console.log('Application started on port', 5000)
+})
