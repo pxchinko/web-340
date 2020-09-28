882077daa7954fbcedd9eb53075ebbd36264f706
Sarah Massie <smassie@my365.bellevue.edu>
2020-09-28 16:25:59 -0700

    master

--- ems/app.js
+++ ems/app.js
@@ -11,69 +11,85 @@
 var express = require("express");
 var logger = require("morgan");
 var helmet = require("helmet");
-var cookieParser = require("cookie-parser");
-var csrf = require("csurf");
 var http = require("http");
 var path = require("path");
 var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+var csrf = require("csurf");
+
 var csrfProtection = csrf({cookie: true});
 
 var Employee = require("./models/employees.js");
-const employees = require("./models/employees.js");
 
 var app = express();
 
-// database connection
-var conn = "mongodb+srv://admin:MonkeyPassword@buwebdev-cluster-1.eate3.mongodb.net/test";
-
-mongoose.connect(conn, {
-  useUnifiedTopology: true,
-  useNewUrlParser: true
-}).then(() => {
-  console.log('Connection to the database instance successful')
-}).catch(error => {
-  console.log(`MongoDB Error: ${error.message}`)
-});
-
-mongoose.Promise = global.Promise;
-
-app.use(logger("short"));
 app.use(helmet.xssFilter);
 app.use(bodyParser.urlencoded({
-    extended: true
-  })
-);
+  extended: true
+}));
 app.use(cookieParser());
 app.use(csrfProtection);
 app.use(function(req, res, next) {
-  var token = request.csrfToken();
+  var token = req.csrfToken();
   res.cookie("XSRF-TOKEN", token);
   res.locals.csrfToken = token;
   next();
 });
 
+// database connection
+var mongoDB = "mongodb+srv://secondUser:cwsM4JmuRNZEiN9M@buwebdev-cluster-1.eate3.mongodb.net/test";
+
+mongoose.connect(mongoDB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error: "));
+db.once("open", function() {
+  console.log("Application connected to mLab MongoDB instance");
+});
+
 // set up the views
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(logger("short"));
 
 // Home page
 app.get('/', function(req, res) {
+var employees = [
+  {
+    firstName: "Jotaro",
+    lastName: "Kujo",
+    jobPosition: "Marine Biologist",
+    department: "Marine Biology"
+  },
+  {
+    firstName: "Jonathon",
+    lastName: "JoeStar",
+    jobPosition: "Real Estate Agent",
+    department: "Real Estate"
+  },
+  {
+    firstName: "Hamlet",
+    lastName: "The Cat",
+    jobPosition: "Cat",
+    department: "Gymnastics"
+  },
+  {
+    firstName: "Calvin",
+    lastName: "Tang",
+    jobPosition: "Linguist",
+    department: "Humanities"
+  },
+];
   res.render('index', {
     title: 'EMS | Home',
     employees: employees,
     message: "New Employee Entry Page"
-  });
-});
-
-app.get("/list", function(req, res){
-  Employee.find({}, function(error, employees){
-    if (error) throw error;
-    res.render("list",{
-      title: "EMS | List",
-      employees: employees
-    });
-    console.log(employees);
   });
 });
 
@@ -82,6 +98,17 @@ app.get('/new', function(req, res) {
     title: 'EMS | New Employee'
   });
 });
+
+app.get("/list", function(req, res){
+    Employee.find({}, function(error, employees){
+      if (error) throw error;
+      res.render("list",{
+        title: "EMS | List",
+        employees: employees
+      });
+      console.log(employees);
+    });
+  });
 
 app.get(`/view:queryName`, function(req, res) {
   const queryName = req.params['queryName'];
@@ -107,11 +134,11 @@ app.get(`/view:queryName`, function(req, res) {
 app.post('/process', function(req, res) {
   console.log(req.body.txtName);
   if (!req.body.firstName||!req.body.lastName||!req.body.jobPosition||!req.body.department) {
-    res.status(400).send('Entries must have a name');
+    res.status(400).send('Entries must be filled out correctly.');
     return;
   };
 
-// get request's form data
+  // get request's form data
   var firstName = req.body.firstName;
   var lastName = req.body.lastName;
   var jobPosition = req.body.jobPosition;
@@ -119,7 +146,6 @@ app.post('/process', function(req, res) {
   var employeeName = firstName + " " + lastName;
   console.log(employeeName + ', ' + jobPosition + ', ' + department);
 
-// employee model
   var employee = new Employee({
     firstName: firstName,
     lastName: lastName,
@@ -127,18 +153,14 @@ app.post('/process', function(req, res) {
     department: department
   });
 
-// save employee name
+  // save employee name
   employee.save(function(error) {
-    if (error) {
-      console.log(error);
-      throw error;
-    } else {
-      console.log(employeeName + ' saved successfully!');
-    };
+    if (error) throw error;
+    console.log(employeeName + ' saved successfully!');
+    });
     res.redirect('/list');
-  });
 });
 
-http.createServer(app).listen(3000, function() {
+http.createServer(app).listen(8080, function() {
   console.log("Application started on port", 3000);
 });
