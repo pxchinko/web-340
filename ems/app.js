8a26e816384fe415471468521bf13f3fb822b302
Sarah Massie <smassie@my365.bellevue.edu>
2020-10-02 23:02:54 -0700

    master

--- ems/app.js
+++ ems/app.js
@@ -18,149 +18,157 @@ var bodyParser = require("body-parser");
 var cookieParser = require("cookie-parser");
 var csrf = require("csurf");
 
-var csrfProtection = csrf({cookie: true});
+var csrfProtection = csrf({cookie:true});
+
+// app.set("port", process.env.PORT || 8080);
 
 var Employee = require("./models/employees.js");
 
 var app = express();
+app.use(express.static('/public'))
 
-app.use(helmet.xssFilter);
+// setup csrf protection
+
+var csrfProtection = csrf({cookie: true});
+
+// initialize express
+
+var app = express();
+
+// use statements
+
+app.use(logger("short"));
+
+app.use(helmet.xssFilter());
+
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
 app.use(cookieParser());
 app.use(csrfProtection);
-app.use(function(req, res, next) {
-  var token = req.csrfToken();
-  res.cookie("XSRF-TOKEN", token);
-  res.locals.csrfToken = token;
+
+app.use(function(request, response, next) {
+  var token = request.csrfToken();
+  response.cookie('XSRF-TOKEN', token);
+  response.locals.csrfToken = token;
   next();
 });
 
-// database connection
+var Employee = require("./models/employees.js");
+
+// Database Connection
+// mLab connection
+
 var mongoDB = "mongodb+srv://secondUser:cwsM4JmuRNZEiN9M@buwebdev-cluster-1.eate3.mongodb.net/test";
 
 mongoose.connect(mongoDB, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-
-mongoose.Promise = global.Promise;
-var db = mongoose.connection;
-
-db.on("error", console.error.bind(console, "MongoDB connection error: "));
-db.once("open", function() {
-  console.log("Application connected to mLab MongoDB instance");
+    useMongoClient: true
 });
 
-// set up the views
+mongoose.Promise = global.Promise;
+
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error: "));
+db.once("open", function() {
+    console.log("Application connected to mLab MongoDB instance");
+});
+
+//add employee model
+var employees = new Employee({
+    firstName: "",
+    lastName: "",
+    jobPosition: "",
+    department: ""
+});
+
+//dependencies
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(logger("short"));
 
-// Home page
-app.get('/', function(req, res) {
-var employees = [
-  {
-    firstName: "Jotaro",
-    lastName: "Kujo",
-    jobPosition: "Marine Biologist",
-    department: "Marine Biology"
-  },
-  {
-    firstName: "Jonathon",
-    lastName: "JoeStar",
-    jobPosition: "Real Estate Agent",
-    department: "Real Estate"
-  },
-  {
-    firstName: "Hamlet",
-    lastName: "The Cat",
-    jobPosition: "Cat",
-    department: "Gymnastics"
-  },
-  {
-    firstName: "Calvin",
-    lastName: "Tang",
-    jobPosition: "Linguist",
-    department: "Humanities"
-  },
-];
-  res.render('index', {
-    title: 'EMS | Home',
-    employees: employees,
-    message: "New Employee Entry Page"
-  });
+
+//home route
+app.get("/", function (request, response) {
+    response.render("index", {
+        title: "EMS | Massie",
+        employees: employees,
+        message: "New Employee:"
+    });
 });
 
-app.get('/new', function(req, res) {
-  res.render('new', {
+app.get('/new', function(request, response) {
+  response.render("new", {
     title: 'EMS | New Employee'
   });
 });
 
-app.get("/list", function(req, res){
-    Employee.find({}, function(error, employees){
-      if (error) throw error;
-      res.render("list",{
-        title: "EMS | List",
+app.get("/list",function(request,response){
+  Employee.find({},function(error,employees){
+    if(error) throw error;
+    
+    response.render("list",{
+        title:"EMS | List of Employees",
         employees: employees
-      });
-      console.log(employees);
     });
   });
+});
 
-app.get(`/view:queryName`, function(req, res) {
-  const queryName = req.params['queryName'];
-
-  Employee.find({'name': queryName}, function(error, employees) {
+app.get("/view/:employeeId", function (request, response) {
+  const employeeId = request.params["employeeId"];
+  Employee.find({ _id: employeeId }, function (error, employee) {
     if (error) {
       console.log(error);
       throw error;
     } else {
-      console.log(employees)
-      if (employees.length > 0) {
-        res.render('view', {
-          title: 'EMS | View',
-          employees: employees
-        })
+      if (!!employee) {
+        response.render("view", {
+          title: "EMS | View",
+          employee: employee,
+        });
       } else {
-        res.redirect('/')
+        response.redirect("/");
       }
     }
-  })
+  });
 });
 
-app.post('/process', function(req, res) {
-  console.log(req.body.txtName);
-  if (!req.body.firstName||!req.body.lastName||!req.body.jobPosition||!req.body.department) {
-    res.status(400).send('Entries must be filled out correctly.');
-    return;
-  };
+app.post("/process", function(request, response) {
 
-  // get request's form data
-  var firstName = req.body.firstName;
-  var lastName = req.body.lastName;
-  var jobPosition = req.body.jobPosition;
-  var department = req.body.department;
-  var employeeName = firstName + " " + lastName;
-  console.log(employeeName + ', ' + jobPosition + ', ' + department);
+  // console.log(request.body.txtName);
+  if (!request.body.firstName||!request.body.lastName||!request.body.jobPosition||!request.body.department) {
+      response.status(400).send("Entries must be filled out completely");
+      return;
+  }
 
-  var employee = new Employee({
-    firstName: firstName,
-    lastName: lastName,
-    jobPosition: jobPosition,
-    department: department
+  // get the request's form data
+
+  var firstName = request.body.firstName;
+  var lastName = request.body.lastName;
+  var jobPosition = request.body.jobPosition;
+  var department = request.body.department;
+
+  console.log(firstName+', '+lastName+', '+jobPosition+', '+department);
+
+  // create a employee model
+  let employee = new Employee({
+    firstName,
+    lastName,
+    jobPosition,
+    department
   });
 
-  // save employee name
-  employee.save(function(error) {
+  // save
+  employee.save(function (error) {
     if (error) throw error;
-    console.log(employeeName + ' saved successfully!');
-    });
-    res.redirect('/list');
+    console.log(firstName + " " + lastName + " saved successfully!");
+  });
+
+    response.redirect("/");
 });
 
-http.createServer(app).listen(8080, function() {
-  console.log("Application started on port", 3000);
+
+//create server
+http.createServer(app).listen(8080, function() { 
+  console.log('Application started on port ' + app.get("port")) 
 });
